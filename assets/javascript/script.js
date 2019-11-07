5f3cacc535c60a69cc31a8740088c812d38f3251
sdreyes <sheshechan90@gmail.com>
2019-11-07 07:46:56 -0500

    Hide side bar at 1014px or smaller and add padding to side bar links.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    $(".sidenav").sidenav();
+// $(document).ready(function() {
+//     $(".sidenav").sidenav();
+// });
+
+$("#menu-toggle").click(function(e) {
+  e.preventDefault();
+  $("#wrapper").toggleClass("toggled");
 });
